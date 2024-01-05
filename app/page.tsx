9f4bbceb05f5fea18fbfd42802b2bcfcfbb0d1d9
ashmeetsmonga <ashmeetsmonga@gmail.com>
2024-01-05 22:04:57 +0530

    home page made responsive

--- app/page.tsx
+++ app/page.tsx
@@ -10,13 +10,15 @@ export default function Home() {
   };
 
   return (
-    <div className="w-full h-full flex justify-center items-center flex-col gap-32">
-      <h1 className="text-8xl text-themeDark font-extrabold">Quizzy</h1>
-      <Link href="/categories">
-        <button onClick={startQuiz} className="bg-themeDark py-4 px-8 rounded-full text-xl font-bold text-themeLight hover:bg-themeOrange hover:text-themeLight transition-all hover:scale-110 ">
-          Play
-        </button>
-      </Link>
+    <div className="w-full h-full flex justify-center items-center">
+      <div className="w-[90%] md:w-3/5 lg:w-2/4 2xl:w-1/4 flex flex-col items-center gap-32 bg-themeLight p-10 rounded-lg">
+        <h1 className="text-7xl md:text-8xl text-themeDark font-extrabold">Quizzy</h1>
+        <Link href="/categories">
+          <button onClick={startQuiz} className="bg-themeDark py-4 px-8 rounded-full text-xl font-bold text-themeLight hover:bg-themeOrange hover:text-themeLight transition-all hover:scale-110 ">
+            Play
+          </button>
+        </Link>
+      </div>
     </div>
   );
 }
