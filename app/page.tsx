e31b3c8263b54cc0405c99ddb8d2a1d71140a7fe
ashmeetsmonga <ashmeetsmonga@gmail.com>
2024-01-05 21:36:31 +0530

    ui changes and reset when quiz is started

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,21 @@
-import Image from "next/image";
+"use client";
+import { useQuizDetailsContext } from "@/context/QuizDetailsContext";
 import Link from "next/link";
 
 export default function Home() {
+  const { setScore } = useQuizDetailsContext();
+
+  const startQuiz = () => {
+    setScore(0);
+  };
+
   return (
     <div className="w-full h-full flex justify-center items-center flex-col gap-32">
       <h1 className="text-8xl text-themeDark font-extrabold">Quizzy</h1>
       <Link href="/categories">
-        <button className="bg-themeLight py-4 px-8 rounded-full text-xl font-bold text-themeDark]">Play</button>
+        <button onClick={startQuiz} className="bg-themeDark py-4 px-8 rounded-full text-xl font-bold text-themeLight hover:bg-themeOrange hover:text-themeLight transition-all hover:scale-110 ">
+          Play
+        </button>
       </Link>
     </div>
   );
