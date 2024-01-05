fdd24bd409a2ca59965142e499956b89fbbb7449
ashmeetsmonga <ashmeetsmonga@gmail.com>
2024-01-05 21:44:32 +0530

    categories mapped

--- app/quiz/quiz.constants.ts
+++ app/quiz/quiz.constants.ts
@@ -3,6 +3,16 @@ export const DURATION = 10000;
 export const categoryIdMap = {
   mythology: 20,
   books: 10,
+  movies: 11,
+  television: 14,
+  music: 12,
+  "video games": 15,
+  science: 17,
+  computer: 18,
+  sports: 21,
+  celebrities: 26,
+  history: 23,
+  vehicles: 28,
 };
 
 export const entities = {
