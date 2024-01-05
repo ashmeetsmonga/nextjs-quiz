52184991734cc7d64c69d4af4d7267a762b1d88b
ashmeetsmonga <ashmeetsmonga@gmail.com>
2024-01-05 22:08:38 +0530

    categories page made responsive

--- app/categories/page.tsx
+++ app/categories/page.tsx
@@ -5,10 +5,10 @@ import Link from "next/link";
 
 const Categories = () => {
   return (
-    <div className="w-full h-full flex justify-center">
-      <div className="flex flex-col gap-5 w-3/4 mt-10">
+    <div className="w-full flex justify-center">
+      <div className="flex flex-col gap-5 w-3/4 mt-10 mb-10">
         <h1 className="text-4xl font-bold text-themeDark">Select Category</h1>
-        <div className="grid grid-cols-4 gap-8 w-full">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 w-full">
           {categories.map((category, idx) => (
             <Link key={idx} href="/details">
               <Category name={category.name} image={category.image} />
